70041f548828a77a180a64336ee1007207fab462
lizd <64845489+Lizd-hub@users.noreply.github.com>
2026-03-25 02:34:16 +0800

    Add files via upload

--- cf1084B.cpp
+++ cf1084B.cpp
@@ -4,41 +4,20 @@ using namespace std;
 void solve(){
     int n;
     cin>>n;
-    vector<int> num(100);
+    vector<int> num(n);
+    int ans = n;
+
     for(int i = 0;i < n;i ++){
         cin>>num[i];
-    }
-
-    int ans = 1;
-    int j = 0;
-    while(j < num.size()){
-        
-        if(j + 1 < num.size()){
-
-            cout<<num[num.begin()]<<num[j]<<' '<<num[j + 1]<<endl;
-            
-            if(num[j] >= num[j + 1] && j > 0){ 
-            num.erase(num.begin() + j + 1);
-            if(j == 1) num.erase(num.begin() ,num.begin()+ j - 1);
-            else num.erase(num.begin());
-            j = 0;
-            ans = 1;
-            continue;
+        if(i > 0)
+            if(num[i - 1] > num[i]){
+                ans = 1;
             }
-
-            if(num[j] >= num[j + 1] && j == 0){
-            num.erase(num.begin() + j + 1);
-            j++;
-            continue;
-            }
-        
-        }
-        j++;
-        ans++;
     }
 
     cout<<ans<<endl;
 }
+
 int main(){
     int t;
     cin>>t;
