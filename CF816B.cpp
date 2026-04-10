b25c3569fee62e551a6e827af132398776238ec9
lizd <64845489+2694361535@users.noreply.github.com>
2026-04-10 16:43:17 +0800

    差分+前缀和

--- CF816B.cpp
+++ CF816B.cpp
@@ -6,29 +6,33 @@ using namespace std;
 int ct[200002];
 
 void solve(){
-    int n,k,q;
+    int n,k,q,max;max = 0;
     cin>>n>>k>>q;
-
 
     for(int i = 0;i < n;i ++){
         int l,r;
         cin>>l>>r;
+        ct[l] += 1;ct[r + 1] -= 1;
+        if(r > max) max = r;//记录最大端点，减少循环次数
+    }
 
-        for(int j = l;j <= r;j ++){
-            ct[j]++;
-        }
+    for(int i = 1;i <= max;i ++){//差分实现所有区间更改
+        ct[i] = ct[i - 1] + ct[i];
+    }
+
+    for(int i = 1;i <= 200000;i ++){//前缀和一定要全遍历，否则会出现b - a < 0或者遗漏计数
+//对于配方覆盖大于2的温度加1标记为合适的温度，
+// 那么后续的查询只要查询计数目标区间被标记为合适的温度个数，
+// 求一段区间的计数和->前缀和
+        if(ct[i] >= k) ct[i] = 1 + ct[i - 1];
+        else ct[i] = 0 + ct[i - 1]; 
     }
 
     for(int i = 0;i < q;i ++){
-        int a,b,ans;ans = 0;
+        int a,b;
         cin>>a>>b;
-
-        for(int j = a;j <= b;j ++){
-            if(ct[j] >= k) ans++;
-        }
-        cout<<ans<<'\n';
+        cout<<ct[b] - ct[a - 1]<<'\n';
     }
-
 }
 signed main(){
     ios::sync_with_stdio(false);
