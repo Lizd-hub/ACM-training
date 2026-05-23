ba97e295b41b85fffa617b92072a24ab4638a938
lizd <64845489+2694361535@users.noreply.github.com>
2026-05-23 16:03:19 +0800

    p1090

---
+++ p1090.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#include <queue>
+#include <vector>
+using namespace std;
+#define int long long
+
+const int maxn = 10005;
+priority_queue<int, vector<int>,greater<int>> pq;
+
+signed main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);cout.tie(0);
+
+    int n;
+    cin>>n;
+
+    for(int i = 0;i < n;i ++){
+        int cinx;
+        cin>>cinx;
+        pq.push(cinx);
+
+    }
+
+    int ans = 0;
+    for(int i = 1;i < n;i ++){
+        int x,y;
+        x = pq.top();
+        pq.pop();
+        y = pq.top();
+        pq.pop();
+        int temp = x + y;
+        ans += temp;
+        pq.push(temp);
+    }
+    
+    cout<<ans;
+     
+    return 0;
+}
